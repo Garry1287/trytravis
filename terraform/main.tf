34b81ba023ddf1020377eb4d0072de660ca854b7
Igor Dzyuin <garry1287@gmail.com>
2019-11-26 13:53:16 +0300

    Create VM with provisioners without input vars

---
+++ terraform/main.tf
@@ -0,0 +1,75 @@
+terraform {
+  # Версия terraform
+  required_version = "0.12.16" 
+}
+
+provider "google" {
+  # Версия провайдера
+  version = "2.5.0"
+
+  # ID проекта
+  project = "myprojtest-254911"
+  region  = "europe-west-1"
+}
+
+resource "google_compute_instance" "app" {
+  name         = "reddit-app-terraform"
+  machine_type = "g1-small"
+  zone         = "europe-west1-b"
+  tags         = ["reddit-app"]
+
+  # определение загрузочного диска
+  boot_disk {
+    initialize_params {
+      image = "reddit-base"
+    }
+  }
+
+  # определение сетевого интерфейса
+  network_interface {
+    # сеть, к которой присоединить данный интерфейс
+    network = "default"
+
+    # использовать ephemeral IP для доступа из Интернет
+    access_config {
+    }
+  }
+  metadata = {
+    # путь до публичного ключа
+    ssh-keys = "appuser:${file("~/.ssh/appuser.pub")}"
+  }
+  connection {
+    type        = "ssh"
+    user        = "appuser"
+    agent       = false
+    host         = self.network_interface[0].access_config[0].nat_ip
+    private_key = file("~/.ssh/appuser")
+  }
+  provisioner "file" {
+    source      = "files/puma.service"
+    destination = "/tmp/puma.service"
+  }
+  provisioner "remote-exec" {
+    script = "files/deploy.sh"
+  } 
+}
+
+resource "google_compute_firewall" "firewall_puma" {
+  name = "allow-puma-default"
+
+  # Название сети, в которой действует правило
+  network = "default"
+
+  # Какой доступ разрешить
+  allow {
+    protocol = "tcp"
+    ports    = ["9292"]
+  }
+
+  # Каким адресам разрешаем доступ
+  source_ranges = ["0.0.0.0/0"]
+
+  # Правило применимо для инстансов с перечисленными тэгами
+  target_tags = ["reddit-app"]
+}
+
