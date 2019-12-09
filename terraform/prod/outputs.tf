86d8c20d0911be1ba5ca9fa4576b3b0d638d2cfd
Igor Dzyuin <garry1287@gmail.com>
2019-12-09 15:28:01 +0300

    Prod and Stage dirs

---
+++ terraform/prod/outputs.tf
@@ -0,0 +1,11 @@
+#output "app_external_ip" {
+#value = google_compute_instance.app.network_interface.0.access_config.0.assigned_nat_ip
+#  value = google_compute_instance.app[*].network_interface[*].access_config[0].nat_ip
+#}
+
+#output "lb-static-ip" {
+#value = google_compute_global_address.lb-static-ip.ip_address
+#value = google_compute_global_forwarding_rule.default.ip_address
+#value = google_compute_global_address.lb-static-ip.address
+#value = google_compute_forwarding_rule.http-frontend.ip_address
+#}
